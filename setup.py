dce045d3408fce621bc8e26aac11564e132e8f2b
Francesco Faraone <ffaraone@gaa9.com>
2015-10-06 09:54:53 +0200

    surround event handlers logic with try/except

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='tourbillon-celery',
-    version='0.4',
+    version='0.5',
     packages=find_packages(),
     install_requires=['celery==3.1.8'],
     zip_safe=False,
--- tourbillon/celery/celery2.py
+++ tourbillon/celery/celery2.py
@@ -28,48 +28,56 @@ def get_celery_stats(agent):
     state = app.events.State()
 
     def handle_task_event(event):
-        state.event(event)
-        logger.debug('task event: %s', event)
-        if 'uuid' in event:
-            task = state.tasks.get(event['uuid'])
-            logger.debug('current task: %s', task)
-            if task.state in ['SUCCESS', 'FAILURE'] and task.name is not None:
+        try:
+            state.event(event)
+            logger.debug('task event: %s', event)
+            if 'uuid' in event:
+                task = state.tasks.get(event['uuid'])
+                logger.debug('current task: %s', task)
+                if task.state in ['SUCCESS', 'FAILURE'] and \
+                        task.name is not None:
+                    runtime = task.runtime if task.runtime else 0
+                    data = [{
+                        'measurement': 'tasks',
+                        'tags': {
+                            'worker': task.worker.hostname,
+                            'task_name': task.name,
+                            'state': task.state,
+                        },
+                        'fields': {
+                            'runtime': float(runtime),
+                            'timestamp': float(task.timestamp),
+                            'started': float(task.started)
+                        }
+                    }]
+                    agent.push(data, db_config['name'])
+        except:
+            logger.exception('cannot parse task event')
+
+    def handle_worker_event(event):
+        try:
+            state.event(event)
+            logger.debug('worker event: %s', event)
+            worker_name = event['hostname']
+            inspect = app.control.inspect([])
+            worker_stat = inspect.stats()[worker_name]
+
+            if 'active' in event and event['active'] > 0:
                 data = [{
-                    'measurement': 'tasks',
+                    'measurement': 'workers',
                     'tags': {
-                        'worker': task.worker.hostname,
-                        'task_name': task.name,
-                        'state': task.state,
+                        'name': event['hostname']
                     },
                     'fields': {
-                        'runtime': float(task.runtime if task.runtime else 0),
-                        'timestamp': float(task.timestamp),
-                        'started': float(task.started)
+                        'processed': event['processed'],
+                        'timestamp': float(event['timestamp']),
+                        'active': event['active'],
+                        'mem': worker_stat['rusage']['maxrss'],
                     }
                 }]
                 agent.push(data, db_config['name'])
-
-    def handle_worker_event(event):
-        state.event(event)
-        logger.debug('worker event: %s', event)
-        worker_name = event['hostname']
-        inspect = app.control.inspect([])
-        worker_stat = inspect.stats()[worker_name]
-
-        if 'active' in event and event['active'] > 0:
-            data = [{
-                'measurement': 'workers',
-                'tags': {
-                    'name': event['hostname']
-                },
-                'fields': {
-                    'processed': event['processed'],
-                    'timestamp': float(event['timestamp']),
-                    'active': event['active'],
-                    'mem': worker_stat['rusage']['maxrss'],
-                }
-            }]
-            agent.push(data, db_config['name'])
+        except:
+            logger.exception('cannot parse worker event')
 
     with app.connection() as connection:
         Receiver = app.subclass_with_self(TourbillonReceiver,

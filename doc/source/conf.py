39a113049527be78e1a37b3967ab1fe6891ac94e
Francesco Faraone <ffaraone@gaa9.com>
2015-11-02 17:51:33 +0100

    fix doc config fix sample config file

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -235,7 +235,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'TourbillonCeleryPlugin.tex', 'Tourbillon Celery Plugin Documentation',
+  (master_doc, 'TourbillonCeleryPlugin.tex', 'tourbillon-celery Documentation',
    'The Tourbillon Team', 'manual'),
 ]
 
@@ -265,7 +265,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'tourbillonceleryplugin', 'Tourbillon Celery Plugin Documentation',
+    (master_doc, 'tourbillonceleryplugin', 'tourbillon-celery Documentation',
      [author], 1)
 ]
 
@@ -279,7 +279,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'TourbillonCeleryPlugin', 'Tourbillon Celery Plugin Documentation',
+  (master_doc, 'TourbillonCeleryPlugin', 'tourbillon-celery Documentation',
    author, 'TourbillonCeleryPlugin', 'One line description of project.',
    'Miscellaneous'),
 ]
